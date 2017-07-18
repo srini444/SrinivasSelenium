4be41f610e199d14ca2dbf340195b2417de3e9f2
srini444 <srinivaschari.y@gmail.com>
2017-07-18 14:51:02 +0530

    Srini

---
+++ src/Class2/Sri.java
@@ -0,0 +1,19 @@
+package Class2;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Sri {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
